4c735a5b4d0e598c87805c854cbead5aac5a5a1a
Nurio Fernández <personal@nurio.me>
2020-12-19 16:49:44 +0100

    Replaced class to className at the Navbar component

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <div class="blog-masthead">
-            <div class="container">
-                <nav class="nav blog-nav">
-                    <a class="nav-link active" href="https://nurio.me/">HOME</a>
-                    <a class="nav-link" href="https://nurio.me/projects">PROJECTS</a>
-                    <a class="nav-link" href="https://nurio.me/aboutme">ABOUT ME</a>
-                    <a class="nav-link" href="https://nurio.me/contact">CONTACT</a>
+        <div className="blog-masthead">
+            <div className="container">
+                <nav className="nav blog-nav">
+                    <a className="nav-link active" href="https://nurio.me/">HOME</a>
+                    <a className="nav-link" href="https://nurio.me/projects">PROJECTS</a>
+                    <a className="nav-link" href="https://nurio.me/aboutme">ABOUT ME</a>
+                    <a className="nav-link" href="https://nurio.me/contact">CONTACT</a>
                 </nav>
             </div>
         </div>
