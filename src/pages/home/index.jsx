7bb06f183230b253c2bcc7622ca1fda6401df184
Nurio Fernández <personal@nurio.me>
2020-12-19 18:19:40 +0100

    Upgraded home page images quality

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,12 +8,12 @@ function Home() {
             <h5>Also here you have some amazing photos.</h5>
             <hr />
             <blockquote className="blockquote text-center">
-                <a href="https://www.instagram.com/p/Bnnpy56FJZW/" target="_blank"><img className="image-home" src="https://i.imgur.com/d5vtMROs.jpg" /></a>
-                <a href="https://www.instagram.com/p/BmLRFQAFCkX/" target="_blank"><img className="image-home" src="https://i.imgur.com/5NR8pmds.jpg" /></a>
-                <a href="https://www.instagram.com/p/Bnjo4J_F24i/" target="_blank"><img className="image-home" src="https://i.imgur.com/kzsORZjs.jpg" /></a>
-                <a href="https://www.instagram.com/p/BqH5APalLdL/" target="_blank"><img className="image-home" src="https://i.imgur.com/yrkESQcs.jpg" /></a>
-                <a href="https://www.instagram.com/p/BkRi3H3loED/" target="_blank"><img className="image-home" src="https://i.imgur.com/49BMtRxs.jpg" /></a>
-                <a href="https://www.instagram.com/p/By_Fx3fBS2x/" target="_blank"><img className="image-home" src="https://i.imgur.com/wgZVyjRs.jpg" /></a>
+                <a href="https://www.instagram.com/p/Bnnpy56FJZW/" target="_blank"><img className="image-home" src="https://i.imgur.com/d5vtMROm.jpg" /></a>
+                <a href="https://www.instagram.com/p/BmLRFQAFCkX/" target="_blank"><img className="image-home" src="https://i.imgur.com/5NR8pmdm.jpg" /></a>
+                <a href="https://www.instagram.com/p/Bnjo4J_F24i/" target="_blank"><img className="image-home" src="https://i.imgur.com/kzsORZjm.jpg" /></a>
+                <a href="https://www.instagram.com/p/BqH5APalLdL/" target="_blank"><img className="image-home" src="https://i.imgur.com/yrkESQcm.jpg" /></a>
+                <a href="https://www.instagram.com/p/BkRi3H3loED/" target="_blank"><img className="image-home" src="https://i.imgur.com/49BMtRxm.jpg" /></a>
+                <a href="https://www.instagram.com/p/By_Fx3fBS2x/" target="_blank"><img className="image-home" src="https://i.imgur.com/wgZVyjRm.jpg" /></a>
             </blockquote>
         </div>
     )
