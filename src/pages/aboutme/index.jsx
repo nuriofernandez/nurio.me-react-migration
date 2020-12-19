4dfcb1e2a0302efa7de5d4168f76e925d7e639ea
Nurio Fernández <personal@nurio.me>
2020-12-19 22:32:10 +0100

    Replaced tab characters with spaces at Aboutme page component

--- src/pages/aboutme/index.jsx
+++ src/pages/aboutme/index.jsx
@@ -34,10 +34,10 @@ function Aboutme() {
                     <div id="aboutme-header">
                         <p className="blog-post-meta pt-1">
                             Hello! I'm Nurio, a software developer. I'm experienced mainly with <b>Java</b> but I also know a lot of <b>Javascript</b> and <b>PHP</b> among others.
-							Speaking about technologies or tools, I've experienced with <b>NodeJS</b>, <b>ElectronJS</b>, <b>MySQL</b>, <b>SpringBoot/Hibernate</b> among others.
-							The programming concepts that I'm most passionate about are <b>asynchronous</b>, <b>cryptography</b> and <b>sockets</b>. Programming is <b>fun</b> when you have to <b>solve complex puzzles</b>.
-							That's the reason why I love getting obsessed to get the best solution to each problem.
-						</p>
+                            Speaking about technologies or tools, I've experienced with <b>NodeJS</b>, <b>ElectronJS</b>, <b>MySQL</b>, <b>SpringBoot/Hibernate</b> among others.
+                            The programming concepts that I'm most passionate about are <b>asynchronous</b>, <b>cryptography</b> and <b>sockets</b>. Programming is <b>fun</b> when you have to <b>solve complex puzzles</b>.
+                            That's the reason why I love getting obsessed to get the best solution to each problem.
+                        </p>
                         <p className="blog-post-meta">My anthem: <i>To see a world in a Grain of Sand, and a Heaven in a Wild Flower, hold Infinity in the palm of your hand, and Eternity in an hour.</i> (William Blake)</p>
                     </div>
                 </div>
