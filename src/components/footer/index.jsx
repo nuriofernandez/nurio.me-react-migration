a6c76f6910020be9bb61783aa10a5d9e7d3a63e3
Nurio Fernández <personal@nurio.me>
2020-12-19 16:50:18 +0100

    Replaced class to className at the Footer component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,34 +8,34 @@ import { faGithubSquare, faInstagramSquare, faLinkedin, faSteamSquare, faTwitter
 
 function Footer() {
     return (
-        <div id="footer" class="container blog-footer">
+        <div id="footer" className="container blog-footer">
             <i>Starting from zero is the strength of innovation.</i>
-            <div class="footer-link-area">
-                <a alt="Twitter Profile" class="footer-link" href="https://twitter.com/nuriofernandez" target="_blank">
+            <div className="footer-link-area">
+                <a alt="Twitter Profile" className="footer-link" href="https://twitter.com/nuriofernandez" target="_blank">
                     <FontAwesomeIcon icon={faTwitterSquare} />
                 </a>
 
-                <a alt="Instagram Profile" class="footer-link" href="https://www.instagram.com/nuriofernandez/" target="_blank">
+                <a alt="Instagram Profile" className="footer-link" href="https://www.instagram.com/nuriofernandez/" target="_blank">
                     <FontAwesomeIcon icon={faInstagramSquare} />
                 </a>
 
-                <a alt="Linkedin Profile" class="footer-link" href="https://www.linkedin.com/in/nurio-fernandez-muniz" target="_blank">
+                <a alt="Linkedin Profile" className="footer-link" href="https://www.linkedin.com/in/nurio-fernandez-muniz" target="_blank">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
 
-                <a alt="Github Profile" class="footer-link" href="https://github.com/xxnurioxx" target="_blank">
+                <a alt="Github Profile" className="footer-link" href="https://github.com/xxnurioxx" target="_blank">
                     <FontAwesomeIcon icon={faGithubSquare} />
                 </a >
 
-                <a alt="MyAnimeList Profile" class="footer-link" href="http://myanimelist.net/profile/xXNurioXx" target="_blank">
-                    <img class="img-footer-link" src="https://nurio.me/assets/img/dot.png" width="43.75px" height="43.75px" />
+                <a alt="MyAnimeList Profile" className="footer-link" href="http://myanimelist.net/profile/xXNurioXx" target="_blank">
+                    <img className="img-footer-link" src="https://nurio.me/assets/img/dot.png" width="43.75px" height="43.75px" />
                 </a>
 
-                <a alt="Youtube Channel" class="footer-link" href="https://www.youtube.com/channel/UCc5aie-j2C4iAcOQgkSHLEQ" target="_blank">
+                <a alt="Youtube Channel" className="footer-link" href="https://www.youtube.com/channel/UCc5aie-j2C4iAcOQgkSHLEQ" target="_blank">
                     <FontAwesomeIcon icon={faYoutubeSquare} />
                 </a>
 
-                <a alt="Steam Profile" class="footer-link" href="https://steamcommunity.com/id/xxnurioxx" target="_blank">
+                <a alt="Steam Profile" className="footer-link" href="https://steamcommunity.com/id/xxnurioxx" target="_blank">
                     <FontAwesomeIcon icon={faSteamSquare} />
                 </a>
             </div>
